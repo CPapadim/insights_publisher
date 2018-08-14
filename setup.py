ee0468d9072e5773afbf4d5b3e17b3af42efc9e7
CPapadim <papadimitriou.c@gmail.com>
2018-08-14 12:50:50 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='insights_publisher',
     description="A package to streamline publication workflows for Knowledge Repo and tie published work to GitHub commits.",
-    url='https://github.com/CPapadim/insights_publisher
+    url='https://github.com/CPapadim/insights_publisher',
     version='0.1.0',
     author=version_info['Charalampos Papadimitriou'],
     author_email=version_info['papadimitriou.c@gmail.com'],
