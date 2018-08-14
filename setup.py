7b49133999ec30ec5d1072d81ad63973a265be56
CPapadim <papadimitriou.c@gmail.com>
2018-08-14 12:37:22 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='insights_publisher',
+    description=(
+        " A package to streamline publication workflows for Knowledge Repo and tie published
+          work to GitHub commits."),
+    url='https://github.com/CPapadim/insights_publisher
+    version='0.1.0',
+    author=version_info['Charalampos Papadimitriou'],
+    author_email=version_info['papadimitriou.c@gmail.com'],
+    license='MIT',
+    packages=['insights_publisher'],
+    zip_safe=False,
+    scripts=['publisher']
+)
